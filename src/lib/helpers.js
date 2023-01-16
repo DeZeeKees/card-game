339d1a37bf991b65a5f056e6fbcb707093bcf6a0
DeZeeKees <87043@roc-teraa.nl>
2023-01-16 14:36:43 +0100

    add parse card helper

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,3 +1,41 @@
 export const parseNonPOJO = async (obj) => {
     return await JSON.parse(JSON.stringify(obj));
 }
+
+export const parseCard = async (locals = false, inputCard = false) => {
+
+    let card = {
+        id: null,
+        name: null,
+        description: null,
+        image: null,
+        types: null,
+        rarity: null,
+        cost: null,
+        attack: null,
+        health: null,
+    }
+
+    if(card == false || locals == false) return false;
+
+    card.id = inputCard.collectionId;
+    card.name = inputCard.name;
+    card.description = inputCard.description;
+    card.image = inputCard.image;
+    
+    const typeArray = await inputCard.type.map( async (typeId, index) => {
+        let type = await locals.pb.collection('card_types').getFirstListItem('id="' + typeId + '"');
+        return type.type;
+    })
+    card.types = await Promise.all(typeArray);
+
+    const rarity = await locals.pb.collection('card_rarities').getFirstListItem('id="' + inputCard.rarity + '"');
+    card.rarity = rarity.name
+
+    card.cost = inputCard.cost;
+    card.attack = inputCard.attack;
+    card.health = inputCard.health;
+
+    return card;
+
+}
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,4 @@
-import { parseNonPOJO } from '$lib/helpers.js';
+import { parseNonPOJO, parseCard } from '$lib/helpers.js';
 
 export const load = async ({ locals }) => {
 
@@ -10,18 +10,6 @@ export const load = async ({ locals }) => {
 
     else {
 
-        let card = {
-            id: null,
-            name: null,
-            description: null,
-            image: null,
-            types: null,
-            rarity: null,
-            cost: null,
-            attack: null,
-            health: null,
-        }
-
         const _allCards = await locals.pb.collection('cards').getFullList(200, {
             sort: '-created'
         })
@@ -29,23 +17,7 @@ export const load = async ({ locals }) => {
         const allCards = await parseNonPOJO(_allCards);
         const randomCard = allCards[Math.floor(Math.random() * (allCards.length - 1))]
 
-        card.id = randomCard.collectionId;
-        card.name = randomCard.name;
-        card.description = randomCard.description;
-        card.image = randomCard.image;
-        
-        const typeArray = await randomCard.type.map( async (typeId, index) => {
-            let type = await locals.pb.collection('card_types').getFirstListItem('id="' + typeId + '"');
-            return type.type;
-        })
-        card.types = await Promise.all(typeArray);
-
-        const rarity = await locals.pb.collection('card_rarities').getFirstListItem('id="' + randomCard.rarity + '"');
-        card.rarity = rarity.name
-
-        card.cost = randomCard.cost;
-        card.attack = randomCard.attack;
-        card.health = randomCard.health;
+        const card = await parseCard(locals, randomCard);
 
         return {
             cardData: card,
